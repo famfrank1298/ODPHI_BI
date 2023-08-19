f0dbbef8b828589fc6e83d301c470f0b870f9703
famfrank1298 <68818256+famfrank1298@users.noreply.github.com>
2023-08-18 20:59:40 -0500

    home page created, carousel/pillars implemented

---
+++ odphi/src/app/Carousel.tsx
@@ -0,0 +1,35 @@
+import { useEffect, useState } from 'react';
+import { Plus_Jakarta_Sans, Manrope } from "next/font/google";
+const jakarta = Plus_Jakarta_Sans({ subsets: ["latin"] });
+const inter = Manrope({ subsets: ["latin"] });
+
+export default function Carousel() {
+    const images = [
+        '/carousel/img1.png',
+        '/carousel/img2.png',
+        '/carousel/img3.png',
+    ];
+
+    const [currentImageIndex, setCurrentImageIndex] = useState(0);
+    const [fade, setFade] = useState(false);
+
+    useEffect(() => {
+        const timer = setInterval(() => {
+            setFade(true); // Start the fading effect
+            setTimeout(() => {
+              setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
+              setFade(false); // Reset the fading effect
+            }, 500); // Wait for 500ms before changing the image
+          }, 5000);
+
+        return () => {
+        clearInterval(timer);
+        };
+    }, [images]);
+
+    return (
+        <div className={`image-carousel ${fade ? 'fade' : ''}`}>
+            <img src={images[currentImageIndex]} alt={`Image ${currentImageIndex}`} />
+        </div>
+    );
+}
---
+++ odphi/src/app/Pillar.tsx
@@ -0,0 +1,43 @@
+
+export default function Pillar() {
+    return (
+        <div className="flex w-full ml-[10rem] mr-[10rem] mb-[5rem]">
+            <div className="container flex w-full mt-[3rem]">
+                <div className="cta">
+                <img className="pillar-img" src="/pillars/unity.JPG" alt="UNITY"/>
+                <div className="text">
+                    <h2 className="mott_header font-semibold text-[30px]">UNITY</h2>
+                    <p>A Brother of Omega Delta Phi is united with his Brothers. Unity fosters a sense of belonging, cooperation, and collective strength, enabling individuals to achieve common goals more effectively while promoting a supportive environment.</p>
+                </div>
+                </div>
+            </div>
+            <div className="container flex w-full mt-[3rem]">
+                <div className="cta">
+                <img className="pillar-img" src="/pillars/honesty.JPG" alt="HONESTY"/>
+                <div className="text">
+                    <h2 className="mott_header font-semibold text-[30px]">HONESTY</h2>
+                    <p>A Knight of Omega Delta Phi is a honest. Honesty establishes trust, transparency, and authenticity within relationships and interactions, creating a foundation for open communication and genuine connections among individuals</p>
+                </div>
+                </div>
+            </div>
+            <div className="container flex w-full mt-[3rem]">
+                <div className="cta">
+                    <img className="pillar-img" src="/pillars/integrity.JPG" alt="INTEGRITY"/>
+                    <div className="text">
+                        <h2 className="mott_header font-semibold text-[30px]">INTEGRITY</h2>
+                        <p>Brothers of Omega Delta Phi upholds moral principles, consistency, and ethical behavior, ensuring that actions align with values and contributing to a reputation of reliability and respect in both personal and professional endeavors.</p>
+                    </div>
+                </div>
+            </div>
+            <div className="container flex w-full mt-[3rem]">
+                <div className="cta">
+                <img className="pillar-img" src="/pillars/leadership.JPG" alt="LEADERSHIP"/>
+                <div className="text">
+                    <h2 className="mott_header font-semibold text-[30px]">LEADERSHIP</h2>
+                    <p>A Brother of Omega Delta Phi leads by example. Leadership empowers individuals to guide, inspire, and influence others towards positive change and growth, fostering innovation, direction, and the development of strong teams and communities.</p>
+                </div>
+                </div>
+            </div>
+     </div>
+     );
+}
