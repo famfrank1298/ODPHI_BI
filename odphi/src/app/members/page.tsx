402945a45de959ac62146cca9b377b95c2cf6383
famfrank1298 <68818256+famfrank1298@users.noreply.github.com>
2023-09-25 14:44:14 -0500

    added social page

--- odphi/src/app/members/page.tsx
+++ odphi/src/app/members/page.tsx
@@ -4,13 +4,32 @@ const inter = Manrope({ subsets: ["latin"] });
 import Brothers from "./Brothers";
 import Header from "../Header";
 import Footer from "../Footer";
+import Link from "next/link";
 
 export default function Members() {
     return (
     <main className="flex min-h-screen flex-col items-center">
         <Header/>
         <div className={inter.className + " leading-[5rem] text-[60px] w-[55rem] font-semibold absolute mott-header"}>ONE CULTURE, ANY RACE</div>
-        <Brothers/>
+        {/* <Brothers/> */}
+        <ul>
+            <Link href="#"><li>Founding</li></Link>
+            <Link href="#"><li>Charter</li></Link>
+            <Link href="#"><li>Alpha</li></Link>
+            <Link href="#"><li>Beta</li></Link>
+            <Link href="#"><li>Gamma</li></Link>
+            <Link href="#"><li>Delta</li></Link>
+            <Link href="#"><li>Epsilon</li></Link>
+            <Link href="#"><li>Zeta</li></Link>
+            <Link href="#"><li>Eta</li></Link>
+            <Link href="#"><li>Theta</li></Link>
+            <Link href="#"><li>Iota</li></Link>
+            <Link href="#"><li>Kappa</li></Link>
+            <Link href="#"><li>Lamda</li></Link>
+            <Link href="#"><li>Mu</li></Link>
+            <Link href="#"><li>Nu</li></Link>
+            <Link href="#"><li>Xi</li></Link>
+        </ul>
         <Footer/>
     </main>
     )
---
+++ odphi/src/app/socials/page.tsx
@@ -0,0 +1,34 @@
+import { Plus_Jakarta_Sans, Manrope } from "next/font/google";
+const jakarta = Plus_Jakarta_Sans({ subsets: ["latin"] });
+const inter = Manrope({ subsets: ["latin"] });
+import Header from "../Header";
+import Gallery from "../gallery/Gallery";
+import Footer from "../Footer";
+
+export default function Home() {
+
+  const socials: string[] = [
+    "socialGallery/social1.jpg",
+    "socialGallery/social2.jpg",
+    "socialGallery/social3.jpg",
+    "socialGallery/social4.jpg",
+    "socialGallery/social5.jpg",
+    "socialGallery/social6.jpg",
+    "socialGallery/social7.jpg",
+    "socialGallery/social8.JPG"
+  ]
+
+  return (
+    <main className="flex min-h-screen flex-col items-center">
+      <Header/>
+      <div className={inter.className + " leading-[5rem] text-[60px] w-[55rem] font-semibold absolute mott-header"}>ONE CULTURE, ANY RACE</div>
+
+      <div className={inter.className + " leading-[5rem] text-[50px] font-semibold section-header"}>Social</div>
+      <p className={inter.className + " text-[25px] mb-[2rem] ml-[10rem] mr-[10rem] text-center"}>At ODPHI, social engagement fosters an environment where bonds of brotherhood are not only strengthened but also extended to various spheres of influence. 
+      Whether we are collaborating with other student organizations at UIC to host cultural events, partnering with external organizations for community service initiatives, or connecting with brothers from different chapters, these moments of social interaction are pivotal.
+      <br></br><br></br>Through social engagement, we collectively build leadership skills, empowering each other to navigate social dynamics and diverse settings, ultimately strengthening the bond of brotherhood within ourselves and extending it to our broader community and beyond.</p>
+      <Gallery images={socials}/>
+      <Footer/>
+    </main>
+  )
+}
