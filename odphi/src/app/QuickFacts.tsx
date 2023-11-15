7f575cb993c9fa7c86b85ee6318358a22f1d66a3
famfrank1298 <68818256+famfrank1298@users.noreply.github.com>
2023-11-14 20:28:03 -0600

    fixed charter date in quick facts

--- odphi/src/app/QuickFacts.tsx
+++ odphi/src/app/QuickFacts.tsx
@@ -14,7 +14,7 @@ export default function QuickFacts() {
         <b>Philanthropy:</b> Wounded Warriors<br/>
         <b>National Slogan:</b> &quot;One Culture, Any Race&quot;<br/>
         <b>ODPHI Founding Date:</b> November 25th, 1987<br/>
-        <b>UIC Beta Iota Founding Date:</b> April 11th, 2011<br/>
+        <b>UIC Beta Iota Founding Date:</b> April 14th, 2011<br/>
         <b>Pillars:</b> Unity, Honesty, Integrity, and Leadership<br/>
         <b>National Motto:</b> &quot;Crescit Eundo&quot;... It Grows as it Goes<br/>
         <b>National Colors:</b> Scarlet, Silver, and Black (supporting color)<br/>
