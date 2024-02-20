0f51283f74141c7567944f0264e9545cab03fc15
famfrank1298 <68818256+famfrank1298@users.noreply.github.com>
2024-02-19 22:55:36 -0600

    added the links to the footer urls

--- odphi/src/app/Footer.tsx
+++ odphi/src/app/Footer.tsx
@@ -9,16 +9,16 @@ export default function Footer() {
       <img src="/odp_logo.png" alt="ODPHI LOGO"/>
     
       <div className="row">
-          <a href="#"><b>National Website</b>: www.omegadeltaphi.com</a>
-          <a href="#"><b>UIC Instagram</b>: instagram.com/chicagoknights87/</a>
-          <a href="#"><b>National Instagram</b>: www.instagram.com/odphinationals/</a>
-          <a href="#">Contact <b>Johnny Reyes</b> (773-971-1170) to Learn More</a>
-          <div>Designed By: <b>Frank Mensah</b> (Sir Asante: 773-875-4389)</div>
+          <a href="https://www.omegadeltaphi.com/" target="_blank"><b>National Website</b>: www.omegadeltaphi.com</a>
+          <a href="https://www.instagram.com/chicagoknights87/" target="_blank"><b>UIC Instagram</b>: instagram.com/chicagoknights87/</a>
+          <a href="https://www.instagram.com/odphinationals/" target="_blank"><b>National Instagram</b>: www.instagram.com/odphinationals/</a>
+          <a href="#">Contact <b>Johnny Reyes</b> (jreye28@uic.edu) to Learn More</a>
+          <div>Designed By: <b>Frank Mensah</b> (Sir Asante: fmensa2@uic.edu)</div>
       </div>
 
 
       <div className="text-center w-full copyright">
-        © Copyright 2023 Omega Delta Phi Fraternity, Inc. | All Rights Reserved
+        © Copyright 2024 Omega Delta Phi Fraternity, Inc - Beta Iota. | All Rights Reserved
       </div>
     </footer>
   )
