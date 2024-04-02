1cdb876bdbe559a1c0297351faecff4eaf0d7916
famfrank1298 <68818256+famfrank1298@users.noreply.github.com>
2024-04-02 13:14:30 -0500

    added the sign up option

--- odphi/src/app/cta/registerForm.tsx
+++ odphi/src/app/cta/registerForm.tsx
@@ -1,133 +1,17 @@
 
-// import './registerForm.css';
+import './registerForm.css';
 
-// export default function RegisterForm() {
+export default function RegisterForm() {
 
-//   return (
-//     <div className="form-container">
-//       <h2 className="heading-secondary">Lets Meet Up!</h2>
-//       <p className="cta-text">
-//         This was only a glimpse into the life of Omege Delta Phi's Mu
-//         Line. If you would like to join or just hang out with us, let us
-//         know!
-//       </p>
-
-//       <form className="cta-form" action="#">
-//         <div>
-//           <label htmlFor="full-name">Full Name</label>
-//           <input
-//           id="full-name"
-//           type="text"
-//           placeholder="John Smith"
-//           required
-//         />
-//       </div>
-
-//       <div>
-//         <label htmlFor="email">Email address</label>
-//         <input
-//           id="email"
-//           type="email"
-//           placeholder="me@uic.edu"
-//           required
-//         />
-//       </div>
-
-//       <div>
-//         <label htmlFor="select-where">How did you find us?</label>
-//         <select id="select-where" required>
-//           <option value="">Please choose one option:</option>
-//           <option value="member">Member</option>
-//           <option value="social">Social Media</option>
-//           <option value="campus">On Campus</option>
-//           <option value="fair">Involvement Fair</option>
-//           <option value="others">Others</option>
-//         </select>
-//       </div>
-
-//       <button className="btn btn--form">Link</button>
-//     </form>
-//     </div>
-//   )
-// //   return (
-// //     <section id="cta form" className="section-cta">
-// //         <div className="container">
-// //           <div className="cta">
-// //             <div className="cta-text-box">
-// //               <h2 className="heading-secondary">Lets Meet Up!</h2>
-// //               <p className="cta-text">
-// //                 This was only a glimpse into the life of Omege Delta Phi's Mu
-// //                 Line. If you would like to join or just hang out with us, let us
-// //                 know!
-// //               </p>
-
-// //               <form className="cta-form" action="#">
-// //                 <div>
-// //                   <label htmlFor="full-name">Full Name</label>
-// //                   <input
-// //                     id="full-name"
-// //                     type="text"
-// //                     placeholder="John Smith"
-// //                     required
-// //                   />
-// //                 </div>
-
-// //                 <div>
-// //                   <label htmlFor="email">Email address</label>
-// //                   <input
-// //                     id="email"
-// //                     type="email"
-// //                     placeholder="me@uic.edu"
-// //                     required
-// //                   />
-// //                 </div>
-
-// //                 <div>
-// //                   <label htmlFor="select-where">How did you find us?</label>
-// //                   <select id="select-where" required>
-// //                     <option value="">Please choose one option:</option>
-// //                     <option value="member">Member</option>
-// //                     <option value="social">Social Media</option>
-// //                     <option value="campus">On Campus</option>
-// //                     <option value="fair">Involvement Fair</option>
-// //                     <option value="others">Others</option>
-// //                   </select>
-// //                 </div>
-
-// //                 <button className="btn btn--form">Link</button>
-// //               </form>
-// //             </div>
-// //             <div className="cta-img-box" role="img" aria-label="the mu line"></div>
-// //           </div>
-// //         </div>
-// //       </section>
-// //   )
-// }
-
-// import Button from 'react-bootstrap/Button';
-// import Form from 'react-bootstrap/Form';
-
-// export default function registerForm() {
-//   return (
-//     <Form>
-//       <Form.Group className="mb-3" controlId="formBasicEmail">
-//         <Form.Label>Email address</Form.Label>
-//         <Form.Control type="email" placeholder="Enter email" />
-//         <Form.Text className="text-muted">
-//           We'll never share your email with anyone else.
-//         </Form.Text>
-//       </Form.Group>
-
-//       <Form.Group className="mb-3" controlId="formBasicPassword">
-//         <Form.Label>Password</Form.Label>
-//         <Form.Control type="password" placeholder="Password" />
-//       </Form.Group>
-//       <Form.Group className="mb-3" controlId="formBasicCheckbox">
-//         <Form.Check type="checkbox" label="Check me out" />
-//       </Form.Group>
-//       <Button variant="primary" type="submit">
-//         Submit
-//       </Button>
-//     </Form>
-//   );
-// }
+  return (
+    <div className="form-container">
+      <div>
+      <h2 className="heading-secondary">Lets Meet Up!</h2>
+      <p className="cta-text">
+        Interested in knowing more about our organization and brothers, than please sign up below so we can get in touch!
+      </p>
+      <button className="button-86" role="button"><a href="https://docs.google.com/forms/d/1l2C198G1u_z8RUCwOnnU7A_qiwyiGnsiNYMQrHQGz5I/edit?ts=660b4571" target="_blank">Sign Here</a></button>
+      </div>
+    </div>
+  )
+}
