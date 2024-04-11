e24ec6b069c567f7fa96be2d3dc3b1ea69f6a7ca
famfrank1298 <68818256+famfrank1298@users.noreply.github.com>
2024-04-11 15:00:26 -0500

    added responsiveness to gallery page

--- odphi/src/app/gallery/Gallery.tsx
+++ odphi/src/app/gallery/Gallery.tsx
@@ -1,37 +1,3 @@
-// "use client"
-
-// import { useEffect, useState } from 'react';
-// import { Plus_Jakarta_Sans, Manrope } from "next/font/google";
-// import Image from 'next/image';
-// const jakarta = Plus_Jakarta_Sans({ subsets: ["latin"] });
-// const inter = Manrope({ subsets: ["latin"] });
-
-// export default function Gallery(props: { images: string[]; }) {
-
-//     const [currentImageIndex, setCurrentImageIndex] = useState(0);
-//     const [fade, setFade] = useState(false);
-
-//     useEffect(() => {
-//         const timer = setInterval(() => {
-//             setFade(true); // Start the fading effect
-//             setTimeout(() => {
-//               setCurrentImageIndex((prevIndex) => (prevIndex + 1) % props.images.length);
-//               setFade(false); // Reset the fading effect
-//             }, 500); // Wait for 300ms before changing the image
-//           }, 2000);
-
-//         return () => {
-//         clearInterval(timer);
-//         };
-//     }, [props.images]);
-
-//     return (
-//         <div className={`gallery ${fade ? 'fade' : ''}`}>
-//             <img className="galleryImg" src={props.images[currentImageIndex]} alt={`Image ${currentImageIndex + 1}`} />
-//         </div>
-//     );
-// }
-
 'use client';
 import React from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
