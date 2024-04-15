135ece470bc844d83f3c8638fe398d83bea00768
famfrank1298 <68818256+famfrank1298@users.noreply.github.com>
2024-04-15 16:18:39 -0500

    redesigned footer

--- odphi/src/app/Footer.tsx
+++ odphi/src/app/Footer.tsx
@@ -9,14 +9,10 @@ export default function Footer() {
       <img src="/odp_logo.png" alt="ODPHI LOGO"/>
     
       <div className="row">
-          <a href="https://www.omegadeltaphi.com/" target="_blank" className="border-right"><b>National Website</b></a>
+          <a href="https://www.omegadeltaphi.com/" target="_blank"><b>National Website</b></a>
           <a href="https://www.instagram.com/chicagoknights87/" target="_blank"><b>UIC Instagram</b></a>
-          <a href="https://www.instagram.com/odphinationals/" target="_blank" className="border-left"><b>National Instagram</b></a>
-          <a href="#" className="border-right">Contact <b>Johnny Reyes</b> (Sir Wepa) to Learn More</a>
-          <div>Designed By: <b>Frank Mensah</b> (Sir Asante)</div>
-          <a href="https://docs.google.com/forms/d/e/1FAIpQLSeoG4RiHZUZMwbh76QMnDU4-SkFAPjF4n_6ko7XLVb6uT9giw/closedform" className="border-left">Interested in Joining: (<b>Sign Up Here</b>)</a>
+          <a href="https://www.instagram.com/odphinationals/" target="_blank"><b>National Instagram</b></a>
       </div>
-
 
       <div className="text-center w-full copyright">
         © Copyright 2024 Omega Delta Phi Fraternity, Inc - Beta Iota. | All Rights Reserved
