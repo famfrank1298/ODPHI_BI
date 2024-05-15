211c1f9d2b9a5f3899df5b2348cab19de276e9f4
famfrank1298 <68818256+famfrank1298@users.noreply.github.com>
2024-05-15 13:46:05 -0500

    small style change

--- odphi/src/app/page.tsx
+++ odphi/src/app/page.tsx
@@ -22,7 +22,7 @@ export default function Home() {
       <Header/>
       <Carousel slides={heroImages}/>
 
-      <div className={inter.className + "leading-[5rem] header-title section-header"}>Omega Delta Phi Fraternity Inc. - B.I Chapter!</div>
+      <div className={inter.className + "leading-[5rem] header-title section-header"}>Omega Delta Phi Fraternity Inc - B.I Chapter!</div>
       <p className={inter.className + " header-body"}>Weclcome to Omega Delta Phi Incorporated Beta Iota Chapter at the University of Illinois at Chicago! Here we are more than a fraternity; we are a lifelong commitment to personal growth, academic excellence, leadership development, and community service. Our fraternity stands as a beacon of diversity, inclusion, and unity, proudly championing our principles of <b>Unity</b>, <b>Honesty</b>, <b>Integrity</b>, and <b>Leadership</b>.
       <br/><br/>As you navigate through these digital halls, you&apos;ll discover the incredible impact we have made on campuses and in our communities. Our members are driven by a passion for making a difference and creating lasting friendships. Whether it&apos;s through philanthropic endeavors (<b>Wounded Warriors</b>!), cultural awareness initiatives, or academic support, we strive to leave a positive mark on the world.
       <br/><br/>So, if you&apos;re seeking an opportunity to be part of something larger than yourself, to forge lifelong friendships, and to grow into a leader who makes a difference, you&apos;ve come to the right place. Feel free to learn about our history, meet our brothers, and discover how you can become a part of our enduring legacy.<br/><br/></p>
